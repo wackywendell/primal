aa939500e65cfe14981fd43819768ba72c536c06
Huon Wilson <dbau.pp+github@gmail.com>
2014-11-11 17:19:17 +1100

    docs

---
+++ implementors/core/iter/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['slow_primes'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.uint.html'>uint</a>&gt; for <a class='struct' href='slow_primes/struct.PrimeIterator.html' title='slow_primes::PrimeIterator'>PrimeIterator</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
